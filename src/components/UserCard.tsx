6f5e77335bf9151b806feef9f5b9fdcd55434f29
Inna-decsgr <anjgkwl27@gmail.com>
2024-05-10 04:38:19 +0900

    Change UI

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -7,14 +7,14 @@ type Props = {
   user: SearchUser
 }
 
-export default function UserCard({user: {name, username, image, following, followers}}: Props) {
+export default function UserCard({user: { name, username, image} }: Props) {
+
   return (
-    <Link className='flex items-center w-full rounded-sm border border-neutral-300 mb-2 p-4 bg-white hover:bg-neutral-50' href={`/user/${username}`}>
+    <Link className='flex items-center w-full rounded-sm  mb-2 p-4 bg-neutral-50 hover:bg-white' href={`/user/${username}`}>
       <Avatar image={image} />
-      <div className='text-neutral-500 ml-2'>
+      <div className='text-neutral-500 ml-3'>
         <p className='text-black font-bold leading-4'>{username}</p>
-        <p>{name}</p>
-        <p className='text-sm leading-4'>{`${followers} followers ${following} following`}</p>
+        <p className='mt-1'>{name}</p>
       </div>
     </Link>
   );
